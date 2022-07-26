909eedbcfa07af075db1dbb6cd030873f72fbcbc
Vincent Paone <55220731+vpaone59@users.noreply.github.com>
2022-07-26 19:50:40 -0400

    added google maps functionality
    added function that interacts with google maps

--- WTFBot.py
+++ WTFBot.py
@@ -12,6 +12,9 @@ from tokenize import String
 from discord.ext import commands
 from discord.utils import get
 import random
+import googlemaps
+import requests
+import discord
 
 
 current_directory = os.getcwd()
@@ -25,15 +28,16 @@ if os.path.exists(os.getcwd() + "/FoodBot/config.json"):
 # if the config.json file doesn't exist within the current directory, use the template to create the file
 else:
     print("CONFIG PATH DOES NOT EXIST")
-    configTemplate = {"Token": "", "Prefix": ""}
+    configTemplate = {"TOKEN": "", "PREFIX": "", "GOOGLE": ""}
     with open(os.getcwd() + "/config.json", "w+") as f:
         # dump the configTemplate to the new config.json
         json.dump(configTemplate, f)
         print("NEW CONFIG.JSON CREATED USING PROVIDED TEMPLATE")
 # grab some info from the config file
 TOKEN = configData["TOKEN"]
-prefix = configData["Prefix"]
-
+prefix = configData["PREFIX"]
+googlekey = configData["GOOGLE"]
+gmaps = googlemaps.Client(key=googlekey)
 
 # search for the foodlist.json file in the current directory and load the file
 if os.path.exists(os.getcwd() + "/FoodBot/foodlist.json"):
@@ -58,26 +62,17 @@ client = commands.Bot(command_prefix=prefix)
 # we need this to check the bot connected properly in the on_ready fxn
 full_ready = False
 
+
 # wait for the bot to FULLY connect to the server
 # once bot connects print message in local terminal
-
-
 @client.event
 async def on_ready():
     print('$$$$$ We have logged in as {0.user} $$$$$'.format(client))
     print(f'$$$$$ {client} $$$$$')
-    full_ready = True
 
-    # if full_ready == True:
-    #     print('$$$$$ Loading Cogs...standby $$$$$')
-    #     loadCogs()
-    # else:
-    #     print('$$$$$ Bot not ready, Cogs not loaded $$$$$')
 
 # every time there is a message in any channel in any guild, this runs
 # param: message - The message content that was last sent
-
-
 @client.event
 async def on_message(message):
 
@@ -109,22 +104,59 @@ async def on_message(message):
     # necessary to process anything the bot will do
     await client.process_commands(message)
 
+
+def isempty():
+
+    if len(foodlist) == 0:
+        return 0
+    else:
+        return len(foodlist)
+
 # print a random food item from the foodlist to the channel the command was run in
 
 
 @client.command()
 async def food(ctx):
-    randindex = random.randint(0, len(foodlist)-1)
-    randfood = foodlist[randindex]
-    await ctx.send(f"{randfood}")
-
-# print out all of the items in foodlist to the channel the command was run in
+    if isempty() == 1:
+        randfood = foodlist[0]
+        await ctx.send(f"```{randfood}```")
+    elif isempty() is not 0:
+        randindex = random.randint(0, len(foodlist)-1)
+        randfood = foodlist[randindex]
+        await ctx.send(f"```{randfood}```")
+    else:
+        await ctx.send("```NO ITEMS IN THE FOOD LIST. PLEASE ADD ITEMS USING !addfood **```")
 
 
 @client.command()
+async def foodsearch(ctx, *keywords):
+    kw = ' '.join(keywords)
+    googlefoods(kw)
+
+    with open("./FoodBot/google_results.json", "r+") as f:
+        data = json.load(f)
+
+    name = data["results"][0]["name"]
+    address = data["results"][0]["formatted_address"]
+    status = data["results"][0]["business_status"]
+    open_now = data["results"][0]["opening_hours"]
+    photo_ref_id = data["results"][0]["photos"][0]["photo_reference"]
+    if open_now["open_now"] == True:
+        open_status = "open"
+    else:
+        open_status = "closed"
+    await ctx.send(f"```We found a result near you for {name}. It's located at {address} and is currently {open_status}.```")
+
+    googlefoodphoto(photo_ref_id)
+
+    await ctx.send(file=discord.File(os.getcwd() + "/FoodBot/photo.jpg"))
+
+
+# print out all of the items in foodlist to the channel the command was run in
+@client.command()
 async def getfoodlist(ctx):
 
-    if len(foodlist) == 0:
+    if isempty() == 0:
         await ctx.send("```NO ITEMS IN FOOD LIST```")
     else:
         x = ''
@@ -133,16 +165,15 @@ async def getfoodlist(ctx):
             x += f_but_string + '\n'
         await ctx.send(f"```{x}```")
 
+
 # add a food item to the foodlist.json
 # only admin should be able to run this
 # param: ctx - The context of which the command is entered
 # param: food - The food item/restaurant you want to add to the list
-
-
-@client.command(aliases=['af'])
-@commands.has_permissions(administrator=True)
+@ client.command(aliases=['af'])
+@ commands.has_permissions(administrator=True)
 # command can only be sent 1 time, every 3 seconds, per user.
-@commands.cooldown(1, 3, commands.BucketType.user)
+@ commands.cooldown(1, 3, commands.BucketType.user)
 async def addfood(ctx, *args):
     # check if the food is already in the list
     food = ' '.join(args)
@@ -165,10 +196,10 @@ async def addfood(ctx, *args):
 # only admin should be able to run this
 # param: ctx - The context of which the command is entered
 # param: food - The food item you want to remove from the list
-@client.command(aliases=['rf'])
-@commands.has_permissions(administrator=True)
+@ client.command(aliases=['rf'])
+@ commands.has_permissions(administrator=True)
 # food can only be sent 1 time, every 3 seconds, per user.
-@commands.cooldown(1, 3, commands.BucketType.user)
+@ commands.cooldown(1, 3, commands.BucketType.user)
 async def remfood(ctx, *args):
     food = ' '.join(args)
     print(type(food))
@@ -186,5 +217,41 @@ async def remfood(ctx, *args):
     # if the word isn't in the list
     else:
         await ctx.send("```{} is not in the list```".format(food))
+
+
+def googlefoods(keyword):
+
+    searchurl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
+    # searchradius = "&radius=" + radius
+    # searchfields = "&fields=formatted_address%2Cname%2Crating%2Cbusiness_status%2Copening_hours%2Cgeometry%2Cphotos"
+
+    # by default google will use current IP to find a result
+    url = searchurl + keyword + "&key=" + googlekey
+    print(url)
+    data = requests.get(url).json()
+    print(data)
+
+    with open(os.getcwd() + "/FoodBot/google_results.json", "w+") as f:
+        json.dump(data, f)
+
+
+def googlefoodphoto(photo_reference_id):
+    photo_height = 400
+    photo_width = 400
+    raw_image_data = gmaps.places_photo(
+        photo_reference=photo_reference_id, max_height=photo_height, max_width=photo_width)
+
+    print("PHOTO DATA FOUND")
+    # open or create new file photo.jpg
+    # w: Open a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+    # b: Open in binary mode.
+    f = open('FoodBot/photo.jpg', 'wb')
+    print("PHOTO FILE CREATED")
+    for chunk in raw_image_data:
+        if chunk:
+            f.write(chunk)
+        else:
+            print("ALL CHUNKS LOADED")
+
 
 client.run(TOKEN)
